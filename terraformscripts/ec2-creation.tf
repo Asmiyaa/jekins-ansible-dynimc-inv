4954be36932cbc2abd5028f81dbe272f0894452c
devopss-dev <Devopsasmiya@gmail.com>
2025-03-06 13:25:26 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,12 +2,13 @@ provider "aws"{
 region = "ap-south-1"
 }
 
-resource "aws_instance" "AWSEC2Instance"{
-   ami = "ami-0d682f26195e9ec0f"
-   instance_type = "t2.micro"
-   key_name = "devopsjan2025"
-   security_groups = ["launch-wizard-1"]
-   tags={
-   Name = "terraformserver"
-   }
+resource "aws_instance" "awsinstance"{
+ami = "ami-0d682f26195e9ec0f"
+instance_type = "t2.micro"
+key_name = "asmiyadevopsaws"
+security_groups = ["launch-wizard-2"]
+tags ={
+Name = "tomcatserverss"
+}
+
 }
